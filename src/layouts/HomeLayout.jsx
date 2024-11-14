801c83037261782adbb820418a5e4b3d516d8979
Arman <mdarman365506@gmail.com>
2024-11-14 12:03:10 +0600

    added nav section in three cols

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,15 +1,27 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
     return (
       <div className="font-Poppins">
         <header>
-                <Header></Header>
-                <section className="w-11/12 mx-auto">
-                    <LatestNews></LatestNews>
-                </section>
+          <Header></Header>
+          <section className="w-11/12 mx-auto">
+            <LatestNews></LatestNews>
+          </section>
         </header>
+        <nav>
+          <section className="w-11/12 mx-auto py-2">
+            <Navbar></Navbar>
+          </section>
+            </nav>
+            <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+                <aside className="left col-span-3">Left Navbar</aside>
+                <section className="col-span-6">main contant</section>
+                <aside className="col-span-3">right navbar</aside>
+
+            </main>
       </div>
     );
 };
